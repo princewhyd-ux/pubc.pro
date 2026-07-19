d1b1c1452265ccd3c209613751df89f1f5fdfcb8
princewhyd-ux <princewhyd@gmail.com>
2026-07-19 18:44:47 +0300

    Add files via upload

--- app/src/main/cpp/Player/PlayerMovement.h
+++ app/src/main/cpp/Player/PlayerMovement.h
@@ -5,12 +5,7 @@
 #include <vector>
 #include <cmath>
 #include <string>
-
-// (لضمان الاستقلالية في الاستدعاء)
-#ifndef MAP_MANAGER_H
-enum class ColliderType { STATIC_WALL, STATIC_GROUND, STATIC_PROP, TRIGGER_ZONE };
-struct EnvironmentCollider { BoundingBox bounds; ColliderType type; std::string tag; };
-#endif
+#include "../Physics/CollisionSystem.h" // 🚨 السطر السحري لاستدعاء تعريفات المجسمات
 
 class PlayerMovement {
 public:
